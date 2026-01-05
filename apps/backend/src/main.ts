0c596bb87090b6854a507de7ec585bd8024e4ba2
tnicodemos <tnicodemos@gmail.com>
2026-01-05 13:01:03 -0300

    fix(backend): enable explicit permissive CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for frontend connection
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
